0504846bcf5ff2897d059a5ece8d524ff0b28c17
github Walkdream24 <walkdream24@gmail.com>
2018-10-06 15:10:02 +0900

    Initial Commit

---
+++ messageeee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  messageeee
+//
+//  Created by 中重歩夢 on 2018/10/06.
+//  Copyright © 2018年 Ayumu Nakashige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
